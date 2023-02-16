518ce99f8bdb1bbce238d99324a4a6680c32ea9b
ancilcleetus <ancilcleetus.work@gmail.com>
2023-02-16 22:45:20 +0530

    Added Loss Function

---
+++ CuriousNet/curiousnet/loss.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+A Loss Function measures how good our predictions are.
+We can use it to adjust the parameters of our neural net.
+"""
+import numpy as np
+
+from curiousnet.tensor import Tensor
+
+class Loss:
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
+    
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        raise NotImplementedError
+    
+
+class MSE(Loss):
+    """
+    MSE -> Mean Squared Error
+    We're actually implementing Total Squared Error instead of MSE
+    """
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        return np.sum((predicted - actual) ** 2)
+    
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        return 2 * (predicted - actual)
