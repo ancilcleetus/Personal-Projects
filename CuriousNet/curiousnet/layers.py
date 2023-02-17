d872f76f1fc4418551487f960f401924f4aea231
ancilcleetus <ancilcleetus.work@gmail.com>
2023-02-18 02:58:09 +0530

    Added Linear Layer

--- CuriousNet/curiousnet/layers.py
+++ CuriousNet/curiousnet/layers.py
@@ -8,13 +8,16 @@ For example, a neural net might look like
 
 inputs -> Linear -> Tanh -> Linear -> output
 """
+from typing import Dict
+
 import numpy as np
 
 from curiousnet.tensor import Tensor
 
 class Layer:
     def __init__(self) -> None:
-        pass
+        self.params: Dict[str, Tensor] = {}
+        self.grads: Dict[str, Tensor] = {}
 
     def forward(self, inputs: Tensor) -> Tensor:
         """
@@ -27,4 +30,38 @@ class Layer:
         Backpropagate this gradient through the layer
         """
         raise NotImplementedError
+
+
+class Linear(Layer):
+    """
+    Computes output = inputs @ w + b
+    """
+    def __init__(self, input_size: int, output_size: int) -> None:
+        super().__init__()
+        # inputs will be of shape (batch_size, input_size)
+        # outputs will be of shape (batch_size, output_size)
+        self.params["w"] = np.random.randn(input_size, output_size)
+        self.params["b"] = np.random.randn(output_size)
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        """
+        output = inputs @ w + b
+        """
+        self.inputs = inputs
+        return inputs @ self.params["w"] + self.params["b"]
     
+    def backward(self, grad: Tensor) -> Tensor:
+        """
+        If y = f(x) and x = a * b + c, then
+        dy/da = f'(x) * b
+        dy/db = f'(x) * a
+        dy/dc = f'(x)
+
+        If y = f(x) and x = a @ b + c, then
+        dy/da = f'(x) @ b.T
+        dy/db = a.T @ f'(x)
+        dy/dc = f'(x)
+        """
+        self.grads["b"] = np.sum(grad, axis=0)
+        self.grads["w"] = self.inputs.T @ grad
+        return grad @ self.params["w"].T
