7cfc48bc8d53e4270c4d609ccdc9213e32741090
ancilcleetus <ancilcleetus.work@gmail.com>
2023-02-22 23:13:27 +0530

    Added NeuralNet (nn.py)

--- CuriousNet/README.md
+++ CuriousNet/README.md
@@ -5,7 +5,7 @@
 1. Tensors          ✔️
 2. Loss Functions   ✔️
 3. Layers           ✔️
-4. Neural Nets      ⬜
+4. Neural Nets      ✔️
 5. Optimizers       ⬜
 6. Data             ⬜
 7. Training         ⬜
---
+++ CuriousNet/curiousnet/nn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+A NeuralNet is just a collection of layers.
+It behaves a lot like a layer itself, although
+we are not going to make it one.
+"""
+from typing import Sequence
+
+from curiousnet.tensor import Tensor
+from curiousnet.layers import Layer
+
+
+class NeuralNet:
+    def __init__(self, layers: Sequence[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+    
+    def backward(self, grad: Tensor) -> Tensor:
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+        return grad
