9ace2af05338467dec15e6b60e5c1800735f4289
ancilcleetus <ancilcleetus.work@gmail.com>
2023-03-03 23:10:37 +0530

    loss.py - added comments and docstrings

--- CuriousNet/curiousnet/loss.py
+++ CuriousNet/curiousnet/loss.py
@@ -10,11 +10,57 @@ import numpy as np
 from curiousnet.tensor import Tensor
 
 class Loss:
+    """
+    A base class for defining loss functions and their gradients.
+
+    Attributes:
+    -----------
+    None
+
+    Methods:
+    --------
+    loss(predicted: Tensor, actual: Tensor) -> float:
+        Computes the loss given the predicted and actual outputs.
+
+    grad(predicted: Tensor, actual: Tensor) -> Tensor:
+        Computes the gradient of the loss with respect to the predicted output.
+
+    """
     def loss(self, predicted: Tensor, actual: Tensor) -> float:
-        raise NotImplementedError
+        """
+        Computes the loss given the predicted and actual outputs.
+
+        Parameters:
+        -----------
+        predicted: Tensor
+            The predicted output of the model.
+        actual: Tensor
+            The actual output of the model.
+
+        Returns:
+        --------
+        float
+            The loss value computed using the predicted and actual outputs.
+        """
+        raise NotImplementedError("Subclass must implement abstract method")
     
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
-        raise NotImplementedError
+        """
+        Computes the gradient of the loss with respect to the predicted output.
+
+        Parameters:
+        -----------
+        predicted: Tensor
+            The predicted output of the model.
+        actual: Tensor
+            The actual output of the model.
+
+        Returns:
+        --------
+        Tensor
+            The gradient of the loss with respect to the predicted output.
+        """
+        raise NotImplementedError("Subclass must implement abstract method")
     
 
 class MSE(Loss):
@@ -23,7 +69,37 @@ class MSE(Loss):
     We're actually implementing Total Squared Error instead of MSE
     """
     def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        """
+        Computes the Mean Squared Error (MSE) given the predicted and actual outputs.
+
+        Parameters:
+        -----------
+        predicted: Tensor
+            The predicted output of the model.
+        actual: Tensor
+            The actual output of the model.
+
+        Returns:
+        --------
+        float
+            The MSE value computed using the predicted and actual outputs.
+        """
         return np.sum((predicted - actual) ** 2)
     
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        """
+        Computes the gradient of the Mean Squared Error (MSE) with respect to the predicted output.
+
+        Parameters:
+        -----------
+        predicted: Tensor
+            The predicted output of the model.
+        actual: Tensor
+            The actual output of the model.
+
+        Returns:
+        --------
+        Tensor
+            The gradient of the MSE with respect to the predicted output.
+        """
         return 2 * (predicted - actual)
