cb68f5f3d7b015212199051b5a5a1f4ad73eae9a
ancilcleetus <ancilcleetus.work@gmail.com>
2024-04-01 18:33:47 +0530

    Added try...finally block in Functionality 02: Respond with 200

--- PyHTTPServer/app/main.py
+++ PyHTTPServer/app/main.py
@@ -19,18 +19,19 @@ def main():
     client_socket, client_address = server_socket.accept()
     print(f"Connection established with {client_address}")
 
-    # Read data from the connection
-    request_data = client_socket.recv(1024)  # Up to 1024 bytes of data from the client
-    print("Received data:")
-    print(request_data.decode())  # Decode the received bytes to a string using UTF-8 decoding
-    
-    # Respond with HTTP/1.1 200 OK
-    response = "HTTP/1.1 200 OK\r\n\r\n"  # Response contains Status Line, Empty Response Headers, Empty Response Body
-    client_socket.sendall(response.encode())  # Send the UTF-8 encoded response back to the client
-
-    # Close the connection
-    client_socket.close()
-    server_socket.close()
+    try:
+        # Read data from the connection
+        request_data = client_socket.recv(1024)  # Up to 1024 bytes of data from the client
+        print("Received data:")
+        print(request_data.decode())  # Decode the received bytes to a string using UTF-8 decoding
+        
+        # Respond with HTTP/1.1 200 OK
+        response = "HTTP/1.1 200 OK\r\n\r\n"  # Response contains Status Line, Empty Response Headers, Empty Response Body
+        client_socket.sendall(response.encode())  # Send the UTF-8 encoded response back to the client
+    finally:
+        # Close the connection
+        client_socket.close()
+        server_socket.close()
 
 
 if __name__ == "__main__":
